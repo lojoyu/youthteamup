8c540564e2b1ed7793a4e99d047ff9140c8667d4
lojoyu <sophie200429@gmail.com>
2024-11-21 20:17:25 +0800

    update intro, category page

--- js/postemail.js
+++ js/postemail.js
@@ -6,31 +6,35 @@
         let sendButton = $(this);
         sendButton.prop('disabled', true).html('<span class="loading-spinner"></span>');
 
-        $.ajax({
-            type: 'POST',
-            url: 'https://script.google.com/macros/s/AKfycbwL9vrIHWtbc1fbVQSr1jviVGtjDgcIj_cEsmeVLi5azEZOSDhDWslu69HPoGEwWNZa/exec',
-            data: JSON.stringify({
-                email: email,
-                name: "test",
-            }),                             // 將資料轉為 JSON 字符串
-            contentType: "application/json", // 設定 Content-Type
-            success: function (response) {
-                sendButton.css('background-color', '#71AFEE'); 
-                sendButton.html('<span class="checkmark"></span>');
-
-                setTimeout(function () {
-                    $('#email').val(''); 
-                    sendButton.css('background-color', '#212121');
-                    sendButton.prop('disabled', false).html('<span>揪！</span>');
-                }, 2000);
+        let url = 'https://script.google.com/macros/s/AKfycbwL9vrIHWtbc1fbVQSr1jviVGtjDgcIj_cEsmeVLi5azEZOSDhDWslu69HPoGEwWNZa/exec'
+        fetch(url, {
+            redirect: "follow",
+            method: "POST",
+            headers: {
+                "Content-Type": "text/plain",
             },
-            error: function (xhr, status, error) {
-                // $('#errorMessage').show();
-                // console.error('提交失敗:', error);
-
-                // // 恢復按鈕狀態
-                sendButton.prop('disabled', false).html('<span>揪！</span>');
+            body: JSON.stringify({
+                page: "page",
+                email: email,
+            }),
+        })
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error(`HTTP 錯誤: ${response.status}`);
             }
+        }).then((data) => {
+            sendButton.css('background-color', '#71AFEE'); 
+            sendButton.html('<span class="checkmark"></span>');
+
+            setTimeout(function () {
+                $('#email').val(''); 
+                sendButton.css('background-color', '#212121');
+                sendButton.prop('disabled', false).html('<span>揪！</span>');
+            }, 2000);
+        }).catch((error) => {
+            sendButton.prop('disabled', false).html('<span>揪！</span>');
         });
     });
 
