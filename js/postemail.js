721b580829569951a7d055a4f66299f6a177c941
lojoyu <sophie200429@gmail.com>
2024-11-25 20:21:47 +0800

    index

--- js/postemail.js
+++ js/postemail.js
@@ -1,12 +1,15 @@
 
 (function($) { "use strict";
-    console.log($('#send'))
+
+    
     $(document).on('click', '#send', function (event) {
         const email = $('#email').val();
         let sendButton = $(this);
         sendButton.prop('disabled', true).html('<span class="loading-spinner"></span>');
 
-        let url = 'https://script.google.com/macros/s/AKfycbwL9vrIHWtbc1fbVQSr1jviVGtjDgcIj_cEsmeVLi5azEZOSDhDWslu69HPoGEwWNZa/exec'
+        //let url = 'https://script.google.com/macros/s/AKfycbwL9vrIHWtbc1fbVQSr1jviVGtjDgcIj_cEsmeVLi5azEZOSDhDWslu69HPoGEwWNZa/exec'
+        
+        let url = 'https://script.google.com/macros/s/AKfycbxLMOJ4Ui6J5W6FXByWs-If0xIXmdZBgyaz0dGbKlpsIZWPZM-G2BPOKWaV-Y4pNMmF/exec'
         fetch(url, {
             redirect: "follow",
             method: "POST",
@@ -14,7 +17,7 @@
                 "Content-Type": "text/plain",
             },
             body: JSON.stringify({
-                page: "page",
+                page: document.title,
                 email: email,
             }),
         })
@@ -25,14 +28,18 @@
                 throw new Error(`HTTP 錯誤: ${response.status}`);
             }
         }).then((data) => {
-            sendButton.css('background-color', '#71AFEE'); 
-            sendButton.html('<span class="checkmark"></span>');
-
-            setTimeout(function () {
-                $('#email').val(''); 
-                sendButton.css('background-color', '#212121');
-                sendButton.prop('disabled', false).html('<span>揪！</span>');
-            }, 2000);
+            console.log(data)
+            if (data.result == 'success') {
+                sendButton.css('background-color', '#71AFEE'); 
+                sendButton.html('<span class="checkmark"></span>');
+    
+                setTimeout(function () {
+                    $('#email').val(''); 
+                    sendButton.css('background-color', '#212121');
+                    sendButton.prop('disabled', false).html('<span>揪！</span>');
+                }, 2000);
+            }
+            
         }).catch((error) => {
             sendButton.prop('disabled', false).html('<span>揪！</span>');
         });
