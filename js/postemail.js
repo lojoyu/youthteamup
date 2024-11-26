f0f876a1ea58b0a911755e27c5d5cf2ce38555a2
lojoyu <sophie200429@gmail.com>
2024-11-26 17:39:23 +0800

    change letstalk button

--- js/postemail.js
+++ js/postemail.js
@@ -1,8 +1,28 @@
 
 (function($) { "use strict";
-
+    // $(".cursor-link-blog-post-1")
+	// .on("mouseenter", function() {	 
+	// $('.cursor').addClass("active-blog-post-1")	  
+	// })
+	// .on("mouseleave", function() {	  
+	// $('.cursor').removeClass("active-blog-post-1")	  
+	// })	
+    var verifying = false;
+    $(document).on('mouseenter', '.cursor-link-blog-post-1', function (event){	
+        if (verifying) return; 
+        $('.cursor').addClass("active-blog-post-1")	  
+        $('#send').html('');
+    })
+    $(document).on('mouseleave', '.cursor-link-blog-post-1', function (event){
+        if (verifying) return; 
+        $('.cursor').removeClass("active-blog-post-1")	  
+        $('#send').html('<span>揪！</span>');
+    })
     
     $(document).on('click', '#send', function (event) {
+        verifying = true;
+        $('.cursor').removeClass("active-blog-post-1")	
+
         const email = $('#email').val();
         let sendButton = $(this);
         sendButton.prop('disabled', true).html('<span class="loading-spinner"></span>');
@@ -30,18 +50,23 @@
         }).then((data) => {
             console.log(data)
             if (data.result == 'success') {
-                sendButton.css('background-color', '#71AFEE'); 
+                // // sendButton.css('background-color', '#71AFEE'); 
+                // //linear-gradient(to top left, #FFFBE1,#FFB79B,#FEFACB, #D7E9E5),
+                // sendButton.css('background', 'linear-gradient(to top left, #FFFBE1,#FFB79B,#FEFACB, #D7E9E5),linear-gradient(to top right, #71AFEE,    #FBE281, #F97241')
                 sendButton.html('<span class="checkmark"></span>');
     
                 setTimeout(function () {
                     $('#email').val(''); 
-                    sendButton.css('background-color', '#212121');
+                    // sendButton.css('background','');
+                    // sendButton.css('background-color', '#212121');
                     sendButton.prop('disabled', false).html('<span>揪！</span>');
+                    verifying = false;
                 }, 2000);
             }
             
         }).catch((error) => {
             sendButton.prop('disabled', false).html('<span>揪！</span>');
+            verifying = false;
         });
     });
 
